ac241f2bb883d660969d17dff1a8e2f476eb2791
Matt Parisien <matthewparisien4@gmail.com>
2021-12-07 19:23:28 -0500

    Implement page transition animation

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ import {
 	useLocation,
 } from "react-router-dom";
 import ScrollToTop from "./helpers/ScrollToTop";
+import { TransitionGroup, Transition } from "react-transition-group";
+import gsap from "gsap";
+import $ from "jquery";
 
 function App() {
 	const location = useLocation();
@@ -30,14 +33,10 @@ function App() {
 		menuShow: false,
 	});
 
-	const sections = useRef([]);
+	const transitionRef = useRef(null);
+	const transitionTimeline = useRef(gsap.timeline());
 
-	const textContent = {
-		home: {
-			vision: {},
-			grid: {},
-		},
-	};
+	const sections = useRef([]);
 
 	function toggleMenuState() {
 		setState(prev => ({ ...prev, menuShow: true }));
@@ -59,6 +58,43 @@ function App() {
 		},
 	};
 
+	const onEnterHandler = () => {
+		transitionTimeline.current.play(0);
+
+		transitionTimeline.current
+			.to(transitionRef.current, {
+				duration: 1,
+				x: "0",
+				ease: "Expo.easeInOut",
+			})
+			.to(transitionRef.current, {
+				duration: 1,
+				y: "-100%",
+				ease: "Expo.easeInOut",
+				onComplete: () => {
+					$(transitionRef.current).css({
+						x: "-100%",
+						y: "0",
+					});
+				},
+			});
+	};
+
+	// const onExitHandler = () => {
+	// 	gsap.killTweensOf(node);
+	// 	// Set initial position and styles
+	// 	gsap.set(node, {
+	// 		position: "absolute",
+	// 		left: 0,
+	// 	});
+	// 	// Create the animation for the incoming component
+	// 	gsap.to(node, {
+	// 		duration: 0.4,
+	// 		autoAlpha: 0,
+	// 		x: -100,
+	// 	});
+	// };
+
 	return (
 		<div className='App'>
 			<ThemeProvider theme={themes}>
@@ -69,24 +105,34 @@ function App() {
 				<Header menuState={state.menuShow} toggleMenu={toggleMenuState} />
 
 				<main>
-					<Routes location={location} key={location.pathname}>
-						<Route
-							path='/'
-							element={
-								<Home
-									hoverState={state.isHovering}
-									setHoverState={toggleMenuState}
-									sectionRefs={el => [...sections.current, el]}
+					<TransitionGroup>
+						<div className='transitioner' ref={transitionRef}></div>
+						<Transition
+							timeout={500}
+							key={location.pathname}
+							onEnter={onEnterHandler}
+							// onExit={onExitHandler}
+						>
+							<Routes location={location} key={location.pathname}>
+								<Route
+									path='/'
+									element={
+										<Home
+											hoverState={state.isHovering}
+											setHoverState={toggleMenuState}
+											sectionRefs={el => [...sections.current, el]}
+										/>
+									}
 								/>
-							}
-						/>
-						<Route path='/contact' element={<Contact />} />
-						<Route
-							path='/projects'
-							element={<Projects updateHoverState={updateHoverState} />}
-						/>
-						<Route path='/projects/:id' element={<ProjectItem />} />
-					</Routes>
+								<Route path='/contact' element={<Contact />} />
+								<Route
+									path='/projects'
+									element={<Projects updateHoverState={updateHoverState} />}
+								/>
+								<Route path='/projects/:id' element={<ProjectItem />} />
+							</Routes>
+						</Transition>
+					</TransitionGroup>
 				</main>
 
 				<Footer />
