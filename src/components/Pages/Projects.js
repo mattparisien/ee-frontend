5827255a423d40137cf55d8fe85fa7d6e86da0aa
Matt Parisien <matthewparisien4@gmail.com>
2021-12-17 19:33:39 -0500

    Begin refactoring directory structure

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,21 +1,18 @@
 import React from "react";
-import Section from "../Section";
-import Heading from "../Heading";
-import Container from "../Container";
+import { Section, Heading, Container } from "../index";
 import ListBlogPosts from "../ListBlogPosts";
 
 function Projects(props) {
-
 	const { addToRefs } = props;
 
 	return (
-		<div className="projects-page page-wrap" ref={addToRefs}>
+		<div className='projects-page page-wrap' ref={addToRefs}>
 			<Section bg={"light"}>
 				<Container>
 					<Heading xl>Our work</Heading>
 				</Container>
 				<Section>
-					<Container padding={'5vw'} bg={"light"}>
+					<Container padding={"5vw"} bg={"light"}>
 						<ListBlogPosts />
 					</Container>
 				</Section>
---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Paragraph from "./Text/Paragraph";
+import Heading from "./Heading";
+import Container from "./Containers/Container";
+import Section from "./Section";
+import UnorderedList from "./Lists/UnorderedList";
+import Header from "./Header/Header";
+import Grid from "./Grid/Grid";
+import { Eye, Ear, TextLogo } from "./Vector/Svg";
+
+export {
+	Paragraph,
+	Heading,
+	Container,
+	Section,
+	UnorderedList,
+	Header,
+	Grid,
+	Eye,
+	Ear,
+	TextLogo,
+};
