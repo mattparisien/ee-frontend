41155c39366247330a2a454a543b558ce83645c6
Matt Parisien <matthewparisien4@gmail.com>
2022-02-19 17:33:02 -0500

    Redirect on auth success

--- src/components/Containers/Temp/Visitor/Entry.js
+++ src/components/Containers/Temp/Visitor/Entry.js
@@ -1,33 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { Box, FormGroup, TextField } from "@mui/material";
+import { Box, FormGroup, TextField, Alert } from "@mui/material";
 import Button from "../../../Button/Button";
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Entry() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [success, setSuccess] = useState(false);
 	const [error, setError] = useState(false);
+	const navigate = useNavigate();
 
 	const handleChange = e => {
-		console.log(e.target.value);
+		setError(null);
 		e.target.name === "email"
 			? setEmail(e.target.value)
 			: setPassword(e.target.value);
 	};
 
-	useEffect(() => {
-		console.log(process.env.REACT_APP_API_URL)
-	}, [email, password]);
-
 	const handleSubmit = e => {
+		const validEmail = process.env.REACT_APP_AUTH_EMAIL;
+		const validPassword = process.env.REACT_APP_AUTH_PASSWORD;
+
+		if (email === validEmail && password === validPassword) {
+			setSuccess(true);
+			console.log("success!");
+		} else {
+			setError("Email or password is incorrect");
+		}
+
 		e.preventDefault();
 	};
+
+	useEffect(() => {
+		success && navigate('/')
+	}, [success])
 
 	return (
 		<>
 			<div className='entry-heading'>Enter</div>
-			<Box component='form' sx={{ width: "400px" }} onSubmit={handleSubmit}>
+			<Box component='form' sx={{ width: "400px" }} onSubmit={handleSubmit} s>
 				<FormGroup sx={{ marginBottom: 4 }}>
 					<TextField
 						label='Email'
@@ -53,6 +64,11 @@ function Entry() {
 				<Button variant='contained' color='dark' fullWidth type='submit'>
 					Enter
 				</Button>
+				{error && (
+					<Alert sx={{ marginTop: 2 }} severity='error'>
+						{error}
+					</Alert>
+				)}
 			</Box>
 		</>
 	);
