909de784fa5790065557124849a35427be4100a0
Matt Parisien <matthewparisien4@gmail.com>
2022-03-02 17:31:40 -0500

    Rebuild navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,53 +1,129 @@
-import React, { useEffect, useState } from "react";
-import useResize from "../../helpers/hooks/useResize";
-import useScroll from "../../helpers/hooks/useScrollDir";
-import { TextLogo } from "../index";
-import { StyledHeader, StyledInnerLayout } from "./styles";
-import Navigation from "./Navigation/Navigation";
-import ContainerFluid from "../Containers/ContainerFluid";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from "@mui/icons-material/Menu";
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 export default function Header(props) {
-	const { menuState, toggleMenu, appRefs, addToRefs, headerColor } = props;
-	const [device, setDevice] = useState(null);
-	const [windowWidth] = useResize();
-	const [isHoverable, setHoverable] = useState(true);
-	const [isScrolling, scrollDirection] = useScroll(props.scroller);
 
-	useEffect(() => {
-		if (menuState) {
-			setHoverable(false);
-		} else if (!menuState) {
-			//Add delay for making burger hoverable again --> wait for menu to be off screen
-			setTimeout(() => {
-				setHoverable(true);
-			}, 1500);
-		}
-	}, [menuState]);
 
-	useEffect(() => {
-		if (windowWidth && windowWidth < 700) {
-			setDevice("mobile");
-		} else {
-			setDevice("desktop");
-		}
-	}, [windowWidth]);
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+	const handleOpenNavMenu = event => {
+		setAnchorElNav(event.currentTarget);
+	};
+	const handleOpenUserMenu = event => {
+		setAnchorElUser(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
 
 	return (
-		<StyledHeader
-			isMenuActive={menuState}
-			$isHoverable={isHoverable}
-			color={props.headerColor}
-			ref={addToRefs}
-			id='site-header'
-		>
-			<ContainerFluid height='100%' noVerticalGutter>
-				<div className='logo-wrapper -absolute-center'>
-					<a href='/'>
-						<TextLogo logoRef={addToRefs} />
-					</a>
-				</div>
-				<Navigation />
-			</ContainerFluid>
-		</StyledHeader>
+		// <StyledHeader
+		// 	isMenuActive={menuState}
+		// 	$isHoverable={isHoverable}
+		// 	color={props.headerColor}
+		// 	ref={addToRefs}
+		// 	id='site-header'
+		// >
+		// 	<ContainerFluid height='100%' noVerticalGutter>
+		// 		<div className='logo-wrapper -absolute-center'>
+		// 			<a href='/'>
+		// 				<TextLogo logoRef={addToRefs} />
+		// 			</a>
+		// 		</div>
+		// 		<Navigation />
+		// 	</ContainerFluid>
+		// </StyledHeader>
+		<AppBar position='static'>
+			<Container maxWidth='xl'>
+				<Toolbar disableGutters>
+					<Typography
+						variant='h6'
+						noWrap
+						component='div'
+						sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+					>
+						LOGO
+					</Typography>
+
+					<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+						<IconButton
+							size='large'
+							aria-label='account of current user'
+							aria-controls='menu-appbar'
+							aria-haspopup='true'
+							onClick={handleOpenNavMenu}
+							color='inherit'
+						>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id='menu-appbar'
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "left",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "left",
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: "block", md: "none" },
+							}}
+						>
+							{pages.map(page => (
+								<MenuItem key={page} onClick={handleCloseNavMenu}>
+									<Typography textAlign='center'>{page}</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+					<Typography
+						variant='h6'
+						noWrap
+						component='div'
+						sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+					>
+						LOGO
+					</Typography>
+					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+						{pages.map(page => (
+							<Button
+								key={page}
+								onClick={handleCloseNavMenu}
+								sx={{ my: 2, color: "white", display: "block" }}
+							>
+								{page}
+							</Button>
+						))}
+					</Box>
+
+					
+				</Toolbar>
+			</Container>
+		</AppBar>
 	);
 }
