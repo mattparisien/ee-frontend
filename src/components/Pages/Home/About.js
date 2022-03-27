a5f55dfe7a1c3b756be1f930914677fb69d4077f
Matt Parisien <matthewparisien4@gmail.com>
2022-03-27 18:09:37 -0400

    Push

--- src/components/Pages/Home/About.js
+++ src/components/Pages/Home/About.js
@@ -1,21 +1,54 @@
-import React from 'react'
-import Section from '../../Containers/Section'
-import ContainerFluid from '../../Containers/ContainerFluid'
-import ReactMarkdown from 'react-markdown';
-import Megaphone from '../../Vector/Megaphone';
+import React from "react";
+import Section from "../../Containers/Section";
+import ContainerFluid from "../../Containers/ContainerFluid";
+import ReactMarkdown from "react-markdown";
+import Megaphone from "../../Vector/Megaphone";
 
-function About({aboutText}) {
-  return (
-    <Section data-theme='dark' classes='o-about -padding-lg'>
-    <ContainerFluid classes='-stretchY'>
-      <ReactMarkdown
-        children={aboutText}
-        className='o-text -text-big -split -fadeUp'
-      />
+function About({ aboutText }) {
+	return (
+    <>
+		{/* <Section data-theme='dark' classes='o-about'>
+			<div className='o-about_content -flex'>
+				<div className='o-about_left -padding-lg'>
+          
+					<ContainerFluid>
+          <h2 className="o-h2 -padding-bottom-lg">Who are we?</h2>
+						<ReactMarkdown
+							children={aboutText}
+							className='o-text -text-big -split -fadeUp'
+						/>
+					</ContainerFluid>
+				</div>
+				<div className='o-about_right  -padding-lg'>
+					<ContainerFluid>
+						<Megaphone />
+					</ContainerFluid>
+				</div>
+			</div>
+		</Section> */}
+		<Section data-theme='dark' classes='o-about -padding-lg'>
+			<div className='o-about_content'>
       <Megaphone />
-    </ContainerFluid>
-  </Section>
-  )
+				<div className='o-about_left -padding-lg'>
+          
+					<ContainerFluid>
+          
+          {/* <h2 className="o-h2 -padding-bottom-lg">Who are we?</h2> */}
+						<ReactMarkdown
+							children={aboutText}
+							className='o-text -text-big -split -fadeUp'
+						/>
+					</ContainerFluid>
+				</div>
+				{/* <div className='o-about_right  -padding-lg'>
+					<ContainerFluid>
+						<Megaphone />
+					</ContainerFluid>
+				</div> */}
+			</div>
+		</Section>
+    </>
+	);
 }
 
-export default About
+export default About;
