c8b41340e872fa696bc244279259ae77a4f1c704
Matt Parisien <matthewparisien4@gmail.com>
2022-04-14 15:27:31 -0400

    Fix hero section

--- src/components/Pages/Home/Hero.js
+++ src/components/Pages/Home/Hero.js
@@ -9,46 +9,6 @@ function Hero({ pageHeading }) {
 		return pageHeading && pageHeading.split(" ");
 	}, [pageHeading]);
 
-	const word = theme => ({
-		".c-char": {
-			transform: "translateY(100%)",
-			opacity: 0,
-		},
-		[theme.breakpoints.up("lg")]: {
-			fontSize: "9rem",
-			lineHeight: "9rem",
-		},
-		[theme.breakpoints.up("md")]: {
-			fontSize: "11vw",
-			lineHeight: "11vw",
-		},
-		[theme.breakpoints.down("md")]: {
-			fontSize: "23vw",
-			lineHeight: "23vw",
-		},
-	});
-
-	const word2 = theme => ({
-		".c-char": {
-			transform: "translateY(100%)",
-			opacity: 0,
-		},
-		[theme.breakpoints.up("lg")]: {
-			fontSize: "9rem",
-			lineHeight: "9rem",
-		},
-		[theme.breakpoints.up("md")]: {
-			fontSize: "11vw",
-			lineHeight: "11vw",
-		},
-		[theme.breakpoints.down("md")]: {
-			alignItems: "center",
-			fontSize: "23vw",
-			lineHeight: "23vw",
-			alignSelf: "center !important",
-		},
-	});
-
 	const logo = theme => ({
 		width: "20vw",
 		height: "20vw",
@@ -81,58 +41,57 @@ function Hero({ pageHeading }) {
 		},
 	});
 
-	const inner = theme => ({
-		[theme.breakpoints.down("md")]: {
-			alignItems: "center",
-			textAlign: "center",
-			justifyContent: "center !important"
-		},
-	});
-
 	const containerStyles = theme => ({
-		[theme.breakpoints.down("sm")]: {
-			marginTop: "5rem",
-			height: "80vw !important"
+		display: "flex",
+		flexDirection: "column",
+		position: "relative",
+		maxWidth: "none !important",
+		justifyContent: "space-between",
+		padding: "3rem 0",
+		height: "calc(100vh - 75px)",
+
+		".word": {
+			fontFamily: "Kobe Bold",
+		},
+		".word:nth-of-type(2)": {
+			textAlign: "right",
+		},
+		[theme.breakpoints.up("md")]: {
+			".word": {
+				fontSize: "8rem",
+				lineHeight: "8rem",
+			},
+		},
+		[theme.breakpoints.up("lg")]: {
+			".word": {
+				fontSize: "10rem",
+				lineHeight: "10rem",
+			},
+		},
+
+		[theme.breakpoints.down("md")]: {
+			justifyContent: "center",
+			alignItems: "center",
+			height: "80vw",
+			".word": {
+				textAlign: "center",
+				fontSize: "16vw",
+				lineHeight: "16vw",
+			},
 		},
 	});
 
 	return (
 		<Section data-theme='light' classes='o-hero ' noGutter>
 			<ContainerFluid classes='-stretchX -stretchY' sx={containerStyles}>
-				<Box className='o-hero_inner -relative -stretchX -stretchY' sx={inner}>
-					<Typography
-						variant='h1'
-						className='o-hero_word'
-						sx={word}
-						data-scroll
-						data-scroll-speed={2}
-					>
-						{words && words[0]}
-					</Typography>
-					<Typography
-						sx={word2}
-						variant='h1'
-						className='o-hero_word'
-						data-scroll
-						data-scroll-speed={2}
-					>
-						{words && words[1]}
-					</Typography>
-					<Typography
-						variant='h1'
-						className='o-hero_word'
-						sx={word}
-						data-scroll
-						data-scroll-speed={2}
-					>
-						{words && words[2]}
-					</Typography>
-					<Box className='o-hero_logo' sx={logo}>
-						<div className='inner -relative -stretchX -stretchY'>
-							<DrawnLogo color='dark' />
-							<div className='revealer'></div>
-						</div>
-					</Box>
+				<Typography className='word'>{words && words[0]}</Typography>
+				<Typography className='word'>{words && words[1]}</Typography>
+				<Typography className='word'>{words && words[2]}</Typography>
+				<Box className='o-hero_logo' sx={logo}>
+					<div className='inner -relative -stretchX -stretchY'>
+						<DrawnLogo color='dark' />
+						<div className='revealer'></div>
+					</div>
 				</Box>
 			</ContainerFluid>
 		</Section>
