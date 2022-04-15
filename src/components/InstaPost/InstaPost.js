7494f81ca0b969c9ba992b228f037b97f4da1497
Matt Parisien <matthewparisien4@gmail.com>
2022-04-15 18:00:21 -0400

    Configure basic get request to insta basic display api

--- src/components/InstaPost/InstaPost.js
+++ src/components/InstaPost/InstaPost.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import InstagramEmbed from "react-instagram-embed";
+import $ from "jquery";
+import axios from "axios";
 
 function InstaPost() {
-	return (
-		<div className="hi">hi</div>
-	);
+	useEffect(() => {
+		const baseURL =
+			process.env.REACT_APP_INSTA_ROOT +
+			`/${process.env.REACT_APP_INSTA_MEDIAID}`;
+		const config = {
+			params: {
+				access_token: REACT_APP_INSTA_TOKEN,
+				fields: "media_url,media_type",
+			},
+		};
+
+		axios
+			.get(baseURL, config)
+			.then(res => console.log(res))
+			.catch(err => console.log(err));
+	}, []);
+
+	return <div className='hi'>hi</div>;
 }
 
 export default InstaPost;
