5c454d7daeb768cb238143d8b3478884fb35c5cc
Matt Parisien <matthewparisien4@gmail.com>
2022-04-15 19:01:35 -0400

    Configure video post as well

--- src/components/InstaPost/InstaPost.js
+++ src/components/InstaPost/InstaPost.js
@@ -5,7 +5,8 @@ import variables from "../../styles/scss/_vars.module.scss";
 
 function InstaPost() {
 	const [postData, setPostData] = useState({
-		image: {
+		media: {
+			type: null,
 			src: null,
 			alt: null,
 		},
@@ -15,20 +16,9 @@ function InstaPost() {
 		likes: null,
 	});
 
-	const wrapper = {};
+	const [mediaComponent, setMediaComponent] = useState(null);
 
-	const imageWrapper = {
-		borderRadius: "10px",
-		overflow: "hidden",
-		width: "300px",
-		height: "300px",
-		img: {
-			width: "100%",
-			height: "100%",
-			objectFit: "cover",
-			objectPosition: "center",
-		},
-	};
+	const wrapper = {};
 
 	const linkWrap = {
 		width: "100%",
@@ -50,20 +40,39 @@ function InstaPost() {
 			left: 0,
 			width: "100%",
 			height: "100%",
-			background: `linear-gradient(transparent, ${variables['colors-light']})`,
+			background: `linear-gradient(transparent, ${variables["colors-light"]})`,
 		},
 	};
 
 	useEffect(() => {
-		const data = getInstaPost().then(data =>
+		getInstaPost().then(data =>
 			setPostData(prev => ({
 				...prev,
-				image: { ...prev.image, src: data.data.media_url },
+				media: {
+					type: data.data.media_type,
+					src: data.data.media_url,
+				},
 				permalink: data.data.permalink,
 				caption: data.data.caption,
 			}))
 		);
 	}, []);
+
+	useEffect(() => {
+		if (postData.media.type) {
+			setMediaComponent(
+				postData.media.type !== "VIDEO" ? (
+					<Image src={postData.media.src} />
+				) : (
+					<Video src={postData.media.src} />
+				)
+			);
+		}
+	}, [postData]);
+
+	useEffect(() => {
+		console.log(mediaComponent);
+	}, [mediaComponent]);
 
 	return (
 		<Box className='instaPost-wrapper' sx={wrapper}>
@@ -74,13 +83,7 @@ function InstaPost() {
 				rel='noreferrer'
 				target='_blank'
 			>
-				<Box className='image-wrapper' sx={imageWrapper}>
-					<Box
-						component='img'
-						className='image'
-						src={postData.image.src && postData.image.src}
-					></Box>
-				</Box>
+				{mediaComponent && mediaComponent}
 				<Box className='post-text' sx={text} pt={2}>
 					<Typography sx={caption} className='caption'>
 						{postData.caption && postData.caption}
@@ -90,5 +93,47 @@ function InstaPost() {
 		</Box>
 	);
 }
+
+const Video = ({ src }) => {
+	const videoWrapper = {
+		borderRadius: "10px",
+		overflow: "hidden",
+		width: "300px",
+		height: "300px",
+		video: {
+			width: "100%",
+			height: "100%",
+			objectFit: "cover",
+			objectPosition: "center",
+		},
+	};
+
+	return (
+		<Box className='video-wrapper' sx={videoWrapper}>
+			<Box component='video' controls muted playsInLine src={src}></Box>
+		</Box>
+	);
+};
+
+const Image = ({ src, alt }) => {
+	const imageWrapper = {
+		borderRadius: "10px",
+		overflow: "hidden",
+		width: "300px",
+		height: "300px",
+		img: {
+			width: "100%",
+			height: "100%",
+			objectFit: "cover",
+			objectPosition: "center",
+		},
+	};
+
+	return (
+		<Box className='image-wrapper' sx={imageWrapper}>
+			<Box component='img' className='image' src={src}></Box>
+		</Box>
+	);
+};
 
 export default InstaPost;
