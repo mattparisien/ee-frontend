51382f5828d9f4e4e4112646680a15db192a7f0d
Matt Parisien <matthewparisien4@gmail.com>
2022-05-03 16:57:23 -0400

    Remove dead console statements

--- src/components/Blocks/GalleryBlock.js
+++ src/components/Blocks/GalleryBlock.js
@@ -2,15 +2,19 @@ import { ImageList, ImageListItem } from "@mui/material";
 import React from "react";
 
 function GalleryBlock({ data }) {
-
-console.log(data)
-
 	return (
-		<ImageList variant={data.images.variant} cols={data.images.rows} gap={data.images.gap}>
+		<ImageList
+			variant={data.images.variant}
+			cols={data.images.rows}
+			gap={data.images.gap}
+		>
 			{data.images.media.data.map((image, i) => {
 				return (
 					<ImageListItem key={i}>
-						<img src={image.attributes.formats.medium.url} alt={image.attributes.alternativeText} />
+						<img
+							src={image.attributes.formats.medium.url}
+							alt={image.attributes.alternativeText}
+						/>
 					</ImageListItem>
 				);
 			})}
