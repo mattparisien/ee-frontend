6719d48e477765fdf44784221b32039f36704176
Matt Parisien <matthewparisien4@gmail.com>
2022-05-15 17:17:19 -0400

    Remove commented code

--- src/helpers/shuffleColors.js
+++ src/helpers/shuffleColors.js
@@ -1,27 +1,18 @@
-import variables from "../styles/scss/_vars.module.scss";
-
-export const shuffleColors = () => {
+export const shuffleColors = object => {
 	const getKeyByValue = (object, value) => {
 		return Object.keys(object).find(key => object[key] === value);
 	};
 
 	const colors = [];
 
-	for (const item in variables) {
-		if (
-			item.startsWith("color") &&
-			item !== "colors-light" &&
-			item !== "colors-dark"
-		) {
-			colors.push(variables[item]);
-		}
+	for (const color in object) {
+		colors.push(object[color]);
 	}
 
 	const randomIndex = Math.ceil(Math.random() * colors.length - 1);
 	const randomColorHex = colors[randomIndex];
-	const randomColorName = getKeyByValue(variables, randomColorHex).replace(
-		"colors-",
-		""
-	);
+	const randomColorName = getKeyByValue(object, randomColorHex);
+
+	console.log(randomColorHex, randomColorName);
 	return [randomColorHex, randomColorName];
 };
