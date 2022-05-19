aea5485e436aa43a6edea302db05c1f961fabc51
Matt Parisien <matthewparisien4@gmail.com>
2022-05-19 16:15:16 -0400

    Add max width to grid items on desktop

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "05de2bb0ceea376a"; }
+/******/ 		__webpack_require__.h = function() { return "ba2ed1e4471cbf45"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/templates/projects/components/ProjectGrid/components/ProjectGridItemImage.js
+++ src/components/templates/projects/components/ProjectGrid/components/ProjectGridItemImage.js
@@ -4,10 +4,10 @@ import HoverFrame from "../../../../../HOC/HoverFrame";
 
 function ProjectGridItemImage({ image, ratio, gridNumber }) {
 	const dimensionsDesktop = {
-		1: "md:w-[40vw] md:h-[27vw]",
-		2: "md:w-[30vw] md:h-[37vw]",
-		3: "md:w-[40vw] md:h-[27vw]",
-		4: "md:w-[45vw] md:h-[55vw]",
+		1: "md:w-[40vw] md:h-[27vw] md:max-w-[500px] max-h-[350px]",
+		2: "md:w-[30vw] md:h-[37vw] md:max-w-[400px] max-h-[500px]",
+		3: "md:w-[40vw] md:h-[27vw] md:max-w-[500px] max-h-[350px]",
+		4: "md:w-[45vw] md:h-[55vw] md:max-w-[600px] max-h-[700px]",
 		5: "md:w-[30vw] md:h-[10vw]",
 	};
 
