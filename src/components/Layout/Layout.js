264973e89e8be0ab83bb0bb813bf7524ebde509c
Matt Parisien <matthewparisien4@gmail.com>
2022-05-19 08:13:42 -0400

    Add loading state on page change

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
-import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import DropdownMenu from "../DropdownMenu/DropdownMenu";
 import useGlobalStore from "../../store/globalStore";
 import classNames from "classnames";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import LoadingScreen from "../Loading/LoadingScreen";
 
 function Layout({ children }) {
 	const dropdownActive = useGlobalStore(state => state.dropdownActive);
@@ -14,6 +16,20 @@ function Layout({ children }) {
 		}
 	);
 
+	const router = useRouter();
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		const handleStart = url => {
+			url !== router.pathname ? setLoading(true) : setLoading(false);
+		};
+		const handleComplete = url => setLoading(false);
+
+		router.events.on("routeChangeStart", handleStart);
+		router.events.on("routeChangeComplete", handleComplete);
+		router.events.on("routeChangeError", handleComplete);
+	}, []);
+
 	return (
 		<div className='scroll-wrapper' data-scroll-container>
 			<div className='Layout'>
@@ -22,6 +38,7 @@ function Layout({ children }) {
 				<div className={fadeClasses}>
 					<main className={"main pt-[69px] bg-light"}>{children}</main>
 					<Footer />
+					<LoadingScreen isActive={loading} />
 				</div>
 			</div>
 		</div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,12 @@
-import { useRef } from "react";
 import Layout from "../components/Layout/Layout";
 import "../styles/css/globals.css";
 import "../styles/scss/main.scss";
 
 function MyApp({ Component, pageProps }) {
-	const scrollRef = useRef(null);
+
 
 	return (
-		<Layout ref={scrollRef}>
+		<Layout>
 			<Component {...pageProps} />;
 		</Layout>
 	);
