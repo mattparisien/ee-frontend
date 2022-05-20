7f6f2f5b1e735cb201e39aaedeaea653aaed4c9b
Matt Parisien <matthewparisien4@gmail.com>
2022-05-20 16:18:03 -0400

    Edit cta

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "54fe127321701045"; }
+/******/ 		__webpack_require__.h = function() { return "152aea902bcc264d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Link/Cta.js
+++ src/components/Link/Cta.js
@@ -8,7 +8,7 @@ function Cta({ children, target, href }) {
 		<Link href={href}>
 			<a
 				href={href}
-				className={`${styles.Cta} flex items-center relative after:will-change-width after:w-12 hover:after:w-[calc(100%+2rem)] after:h-12 after:absolute after:top-1/2 after:left-0 after:rounded-[3rem] after:mix-blend-multiply after:-translate-y-1/2 after:-translate-x-3 after:bg-yellow-custom  after:transition-all after:duration-[0.6s] ease-[cubic-bezier(.645,.045,.355,1)]`}
+				className={`${styles.Cta} inline-block relative after:will-change-width after:w-12 hover:after:w-[calc(100%+2rem)] after:h-12 after:absolute after:top-1/2 after:left-0 after:rounded-[3rem] after:mix-blend-multiply after:-translate-y-1/2 after:-translate-x-3 after:bg-yellow-custom  after:transition-all after:duration-[0.6s] ease-[cubic-bezier(.645,.045,.355,1)]`}
 			>
 				<CtaInner text={children} />
 			</a>
@@ -19,8 +19,10 @@ function Cta({ children, target, href }) {
 function CtaInner({ text }) {
 	return (
 		<>
-			<span className={`Cta_Text block text-xl`}>{text}</span>
-			<ArrowForwardIosIcon sx={{ height: "0.8rem" }} />
+			<div className='CtaInner w-full h-full flex items-center'>
+				<span className={`Cta_Text block text-xl`}>{text}</span>
+				<ArrowForwardIosIcon sx={{ height: "0.8rem" }} />
+			</div>
 		</>
 	);
 }
