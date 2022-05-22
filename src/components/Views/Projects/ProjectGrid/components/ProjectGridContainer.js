eb5ceea314c7f0048613ca6c5481860941663dc3
Matt Parisien <matthewparisien4@gmail.com>
2022-05-22 17:51:16 -0400

    Remove views dir

--- src/components/Views/Projects/ProjectGrid/components/ProjectGridContainer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ProjectGridContainer({ children }) {
-	return <div className='ProjectGrid min-h-screen grid grid-cols-12 gap-y-10 relative'>{children}</div>;
-}
-
-export default ProjectGridContainer;
