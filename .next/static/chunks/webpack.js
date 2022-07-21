2a534687bc29129e2bc365a0706a549fda37e648
Matt Parisien <matthewparisien4@gmail.com>
2022-07-21 17:36:56 -0400

    Push

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "38fe8c9022c94fe1"; }
+/******/ 		__webpack_require__.h = function() { return "e06209b23f33fb4e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -37,7 +37,7 @@ function Layout({ children }) {
 					<DropdownMenu />
 					<div className={fadeClasses}>
 						<main
-							className={`main mb-[600px] pb-2 md:pb-20 z-[99] sticky pt-[${
+							className={`main mb-[600px] pb-2 md:pb-20 z-[2]  sticky pt-[${
 								pathname === "/" ? "0" : "69px"
 							}] bg-light relative after:absolute after:top-full after:left-0 after:bg-light after:rounded-b-3xl after:w-full after:h-10`}
 						>
